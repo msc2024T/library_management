e17741eb3e83031facdb0b1a2e4b5cfe04afa525
msc2024T <mohamad.sc66@gmail.com>
2025-09-28 15:43:36 -0400

    Permissions & Custom Logic (Encapsulation & Overriding)

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from .models import Book, AudioBook, EBook
 from .serializers import BookSerializer, EBookSerializer, AudioBookSerializer
@@ -20,6 +21,17 @@ class BookViewSet(viewsets.ModelViewSet):
             return Response({"error": "Book already exists"}, status=status.HTTP_400_BAD_REQUEST)
 
         return super().create(request, *args, **kwargs)
+
+    @action(detail=True, methods=['post'])
+    def mark_unavailable(self, request, pk=None):
+
+        try:
+            book = Book.objects.get(id=pk)
+            book.availability = False
+            book.save()
+            return Response({'message': 'Book marked as unavailable'}, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response({'message': 'Book does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class EBookViewSet(viewsets.ModelViewSet):
